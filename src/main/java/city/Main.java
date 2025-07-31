c0fd2e99e47d26496fced09463d7d248be26cc27
Владислав Кузнецов <kuzn3tsoff.v@yandex.ru>
2025-07-31 16:36:45 +0300

    Media_Lanch

--- src/main/java/city/Main.java
+++ src/main/java/city/Main.java
@@ -7,28 +7,24 @@ public class Main {
     public static void main(String[] args) {
         HelpDesk helpDesk = HelpDesk.getInstance();
         
-        // Initialize banks
         List<Bank> banks = new ArrayList<>();
         for (int i = 1; i <= Config.getBanksCount(); i++) {
             Bank bank = new Bank(i);
             banks.add(bank);
         }
 
-        // Initialize workers
         List<Worker> workers = new ArrayList<>();
         for (int i = 1; i <= Config.getWorkersCount(); i++) {
             Worker worker = new Worker(i);
             workers.add(worker);
         }
 
-        // Initialize spenders
         List<Spender> spenders = new ArrayList<>();
         for (int i = 1; i <= Config.getSpendersCount(); i++) {
             Spender spender = new Spender(i);
             spenders.add(spender);
         }
 
-        // Выводим начальные состояния ДО запуска потоков
         System.out.println("\n=== НАЧАЛЬНОЕ СОСТОЯНИЕ СИСТЕМЫ ===");
         System.out.println("Total money amount in city on day start: " + 
                           helpDesk.calculateTotalMoney(banks, workers, spenders) + "$");
@@ -37,23 +33,19 @@ public class Main {
         spenders.forEach(spender -> System.out.println("Initial " + spender.getInfo()));
         System.out.println("===============================\n");
 
-        // Запускаем потоки ПОСЛЕ вывода начального состояния
         banks.forEach(Thread::start);
         workers.forEach(Thread::start);
         spenders.forEach(Thread::start);
 
-        // Initialize Media
         Media media = new Media(banks, workers, spenders, helpDesk);
         media.start();
 
-        // Остальной код остается без изменений...
         try {
             Thread.sleep(Config.getWorkDayDuration());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Stop all threads
         banks.forEach(Thread::interrupt);
         workers.forEach(Thread::interrupt);
         spenders.forEach(Thread::interrupt);
